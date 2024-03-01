6a7a8bd3b2a9ad1865d601eac9dd5c4b2a35b4b0
Pramod <saipramod542004@gmail.com>
2024-03-01 19:30:51 +0530

    DAY 3 PART !
    Very well done this day cause we completed home page 90%
    Footer is yet to be finished.
    Me and  my beta atul are arguing on one thing
    I want some dark background but he is denying it . Let's see who wins
    
    Co-Authored-By: kishusingh62 <161470980+kishusingh62@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function showVoterForm()
 {
     var loginbar = document.querySelector('.loginbar');
+    var hptext = document.querySelector('.hptext');
     loginbar.innerHTML=`
     <br><br>
             <h2>Login as Voter:</h1>
@@ -14,10 +15,21 @@ function showVoterForm()
             <br>
             <p id="back" onclick="defaultLogin()">Back</p>
     ` 
+    hptext.innerHTML=`
+    <br>
+                <p style="margin-left: 350px; font-size:29px; margin-bottom:-40px"><b>Be a Voter</b></p>
+                
+                <p style="font-size:23px; padding:30px "><b>Explore the profiles of candidates, understand their goals,and cast your <br>vote for the individuals who resonate with your vision for the college.  <br>Your voice matters, and your vote can shape the future of our campus community.</b></p>
+                
+                <img src="images/voter.png" alt="" srcset="" height="250px" style="margin-left: 290px; margin-top:-50px">
+                
+    `
+    
 }
 function showCandidateForm()
 {
     var loginbar = document.querySelector('.loginbar');
+    var hptext = document.querySelector('.hptext');
     loginbar.innerHTML=`
     <br><br>
             <h2>Login as Candidate:</h1>
@@ -34,10 +46,20 @@ function showCandidateForm()
             <br>
             <p id="back" onclick="defaultLogin()">Back</p>
     `
+    hptext.innerHTML=`
+    <br>
+                <p style="margin-left: 350px; font-size:29px; margin-bottom:-40px"><b>Be a Candidate</b></p>
+                
+                <p style="font-size:23px; padding:30px; margin-bottom:0px "><b>Are you passionate about making a difference?<br>Consider standing as a candidate! Showcase your leadership skills, <br>share your ideas, and let your fellow students know why you're the right <br>choice to represent them.</b>
+                </p>
+                <img src="images/candi.png" alt="" srcset="" height="300px" style="margin-left: 290px; margin-top:-30px;">
+                
+    `
 }
 function defaultLogin()
 {
     var loginbar = document.querySelector('.loginbar');
+    var hptext = document.querySelector('.hptext');
     loginbar.innerHTML=`<br><br><br>
     <p style="font-size:27px;"><b>Choose a Login method</b></p>
     
@@ -45,4 +67,10 @@ function defaultLogin()
     <br><br><br>
     <button class="login" onclick="showCandidateForm()" style="font-size: 17px;">Login as Candidate</button>
     ` 
+    hptext.innerHTML=`
+    <p style="font-size:23px; padding:30px "><b>Step into the virtual arena of campus elections – where wit, promises,  <br>and votes collide! <br>Your front-row seat to the coolest show on campus awaits. <br><br>Let the digital democracy party begin!</b>
+                </p>    
+                <br>
+                
+    `
 }
